756dbb5b9c52532e0159e8415d41747b27a10b4b
edena <djaziz12393@gmail.com>
2021-07-06 10:22:14 +0300

    init

---
+++ GitFun.SourceTree.Create/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitFun.SourceTree.Create
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
