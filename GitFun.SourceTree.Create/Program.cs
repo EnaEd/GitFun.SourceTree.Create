1b9bd9b45a7e1c3a5f5fb0ab8d4613f8086cebfe
edena <djaziz12393@gmail.com>
2021-09-22 12:37:55 +0300

    right commit new

--- GitFun.SourceTree.Create/Program.cs
+++ GitFun.SourceTree.Create/Program.cs
@@ -7,7 +7,7 @@ namespace GitFun.SourceTree.Create
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("For fetsh");
+            Console.WriteLine("For fet");
         }
     }
 }
