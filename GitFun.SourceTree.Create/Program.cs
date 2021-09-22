c7593220a052c76a0054a00200e413ff08185cec
edena <djaziz12393@gmail.com>
2021-09-22 10:12:03 +0300

    new commit

--- GitFun.SourceTree.Create/Program.cs
+++ GitFun.SourceTree.Create/Program.cs
@@ -2,11 +2,11 @@
 
 namespace GitFun.SourceTree.Create
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!!");
         }
     }
 }
