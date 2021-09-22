9e27b4a202e5eb9e2811ff5629b61509a80bf576
Ed <35765131+EnaEd@users.noreply.github.com>
2021-09-22 10:56:42 +0300

    Update Program.cs

--- GitFun.SourceTree.Create/Program.cs
+++ GitFun.SourceTree.Create/Program.cs
@@ -6,7 +6,7 @@ namespace GitFun.SourceTree.Create
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
